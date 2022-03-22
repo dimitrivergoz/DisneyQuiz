371c4bb2d247fc3ad9fefc8c804af65fa52740cd
dimitrivergoz <dimitri.vergoz@edu.devinci.fr>
2022-03-22 17:31:40 +0100

    Gros changements avec 1 seule page, génération sur n'importe laquelle des pages

--- app.py
+++ app.py
@@ -9,50 +9,35 @@ response = requests.get('https://api.disneyapi.dev/characters/')
 data = json.loads(response.content)
 
 
-
-list_id = []
-for i in data['data']:
-    list_id.append(i['_id'])
-
-list_name = []
-for i in data['data']:
-    list_name.append(i['name'])
-
-
-def create_link(id):
-    lien = "https://api.disneyapi.dev/characters"+str(id)
-    return lien
-
-#148 pages
 @app.route('/', methods=['GET'])
 def index():
-    lien = "https://api.disneyapi.dev/characters"
-    req = requests.get(lien)#?page=100
-    data = json.loads(req.content)
-    return render_template('home.html', data=data['data'], req=lien)
-
-#Zone de tests pour afficher un élément spécifique
-@app.route('/spe/', methods=['GET'])
-def spe():
-    random_number = random.choice(list_id)
-    random_ajout = random.randint(0,5)
-
-    id = random_number
-    response = requests.get('https://api.disneyapi.dev/characters/'+str(id))
-    data = json.loads(response.content)
-    return render_template('spe.html', name=data['name'], url=data['imageUrl'],data=data,list_name=list_name, list_id=list_id,random_ajout=random_ajout, random_number=random_number)
-
-
-
-@app.route('/page/', methods=['GET'])
-def page():
-    random_page = random.randint(0,149)
+    random_page = random.randint(0,148)
     response = requests.get('https://api.disneyapi.dev/characters?page='+str(random_page))
     data = json.loads(response.content)
     list_id = []
+    list_name = []
+    list_url = []
     for i in data['data']:
-        list_id.append(i['name'])
-    return render_template('page.html', list_id=data )
+        list_id.append(i['_id'])
+        list_name.append(i['name'])
+        try:
+            list_url.append(i['imageUrl'])
+        except KeyError:
+            list_url.append('#')
+    aleatoire_number = random.randint(0,len(list_id))
+    for_false_name_aleatoire_number = random.randint(0,len(list_id))
+    #On vérifie que les 2 nombres générés ne sont pas indiques, de plus, on verifie qu'ils soient pas "out of range"
+    if for_false_name_aleatoire_number == aleatoire_number:
+        if for_false_name_aleatoire_number > len(list_id):
+            for_false_name_aleatoire_number/=2
+        else:
+            for_false_name_aleatoire_number-1
+    id_choisi = list_id[aleatoire_number]
+    name_choisi = list_name[aleatoire_number]
+    quiz_2nd_name = list_name[for_false_name_aleatoire_number]
+    quiz_3nd_name = list_name[-for_false_name_aleatoire_number]
+    url_choisi = list_url[aleatoire_number]
+    return render_template('page.html', id_choisi=id_choisi,list_url=url_choisi,quiz_2nd_name=quiz_2nd_name,quiz_3nd_name=quiz_3nd_name,  name_choisi=name_choisi,aleatoire_number=aleatoire_number,random_page=random_page)
 
 
 if __name__ == '__main__':
