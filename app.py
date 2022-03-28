ce39830a64f0cac49b4977e3bbd02b7352a9b56c
dimitrivergoz <dimitri.vergoz@edu.devinci.fr>
2022-03-28 10:43:50 +0200

    elements inutiles supprimés

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 response = requests.get('https://api.disneyapi.dev/characters/')
 data = json.loads(response.content)
 
-# probleme d'affichage (image) sur #id 2919, 5156
+# probleme d'affichage (image) sur #id 2919, 5156, 6747
 @app.route('/', methods=['GET'])
 def index():
+    #On a 148 pages -> on genere un nombre aléatoire pour choisir la page sur lequel le personne sera choisi
     random_page = random.randint(0,148)
+    #On cherche la page avec le nb aléatoire généré
     response = requests.get('https://api.disneyapi.dev/characters?page='+str(random_page))
     print('PAGE ========>',random_page)
     data = json.loads(response.content)
@@ -20,6 +22,8 @@ def index():
     list_url = []
     list_film = []
     list_pb = []
+    current_list_film = []
+    #On met TOUS les éléments présents sur la page dans des listes
     for i in data['data']:
         list_id.append(i['_id'])
         list_name.append(i['name'])
@@ -32,7 +36,9 @@ def index():
         except KeyError:
             list_film.append('#')
             list_pb.append(i)
+    #On génére encore une fois un nombre aléatoire pour choisir UN personnage qui sera affiché
     aleatoire_number = random.randint(0,len(list_id)-1)
+    #INDEX pour les "faux" noms 
     for_false_name_aleatoire_number = random.randint(0,len(list_id))
     #On vérifie que les 2 nombres générés ne sont pas indiques, de plus, on verifie qu'ils soient pas "out of range"
     if for_false_name_aleatoire_number == aleatoire_number:
@@ -42,15 +48,22 @@ def index():
             for_false_name_aleatoire_number-1
     id_choisi = list_id[aleatoire_number]
     print('PAGE ========>',id_choisi)
+
+    #On met les infos voulues dans des variables pour les afficher
     name_choisi = list_name[aleatoire_number]
     quiz_2nd_name = list_name[for_false_name_aleatoire_number-1]
     quiz_3nd_name = list_name[-for_false_name_aleatoire_number]
     url_choisi = list_url[aleatoire_number]
     nb_film = 0
+    #Compteur pour connaître le nb de films
     for i in list_film:
         if i != []:
             nb_film+=1
-    return render_template('page.html',list_pb=list_pb,nb_film=nb_film,id_choisi=id_choisi,list_film=list_film, list_url=url_choisi,quiz_2nd_name=quiz_2nd_name,quiz_3nd_name=quiz_3nd_name,  name_choisi=name_choisi,aleatoire_number=aleatoire_number,random_page=random_page)
+            if i not in current_list_film:
+                current_list_film.append(i)
+
+    
+    return render_template('index.html',current_list_film=current_list_film,nb_film=nb_film,id_choisi=id_choisi,list_film=list_film, list_url=url_choisi,quiz_2nd_name=quiz_2nd_name,quiz_3nd_name=quiz_3nd_name,  name_choisi=name_choisi,aleatoire_number=aleatoire_number,random_page=random_page)
 
 
 if __name__ == '__main__':
