ec8fc62e46d3e6e334abf43d3d96b008025d0eda
dimitrivergoz <dimitri.vergoz@edu.devinci.fr>
2022-03-28 11:23:04 +0200

    Mise en place d'un aléatoire sur la disposition des noms pour le quiz

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 import requests
 import json
 import random
+import numpy
 app = Flask(__name__)
 
 response = requests.get('https://api.disneyapi.dev/characters/')
@@ -52,7 +53,7 @@ def index():
     #On met les infos voulues dans des variables pour les afficher
     name_choisi = list_name[aleatoire_number]
     quiz_2nd_name = list_name[for_false_name_aleatoire_number-1]
-    quiz_3nd_name = list_name[-for_false_name_aleatoire_number]
+    quiz_3rd_name = list_name[-for_false_name_aleatoire_number]
     url_choisi = list_url[aleatoire_number]
     nb_film = 0
     #Compteur pour connaître le nb de films
@@ -62,8 +63,9 @@ def index():
             if i not in current_list_film:
                 current_list_film.append(i)
 
-    
-    return render_template('index.html',current_list_film=current_list_film,nb_film=nb_film,id_choisi=id_choisi,list_film=list_film, list_url=url_choisi,quiz_2nd_name=quiz_2nd_name,quiz_3nd_name=quiz_3nd_name,  name_choisi=name_choisi,aleatoire_number=aleatoire_number,random_page=random_page)
+    affichage = [name_choisi,quiz_2nd_name, quiz_3rd_name]
+    prompt = numpy.random.choice(affichage, len(affichage), False)
+    return render_template('index.html',prompt=prompt,current_list_film=current_list_film,nb_film=nb_film,id_choisi=id_choisi,list_film=list_film, list_url=url_choisi,quiz_2nd_name=quiz_2nd_name,quiz_3rd_name=quiz_3rd_name,  name_choisi=name_choisi,aleatoire_number=aleatoire_number,random_page=random_page)
 
 
 if __name__ == '__main__':
